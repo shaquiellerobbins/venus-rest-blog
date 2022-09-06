f194c17d00d0a1fbafba2dfd41f2a685e7e0fdde
shaquielle robbins <shaquiellerobbins@gmail.com>
2022-09-06 15:09:46 -0500

    first commit

---
+++ src/test/java/shaq/venusrestblog/VenusRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package shaq.venusrestblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusRestBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
